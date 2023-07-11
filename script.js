ba12f5d5350d11a6bf3c4c199282e5efa9b1a80a
Zvi Schvarcz <zvischvarcz@gmail.com>
2023-07-11 17:24:59 +0100

    revamped js, added favicon

--- script.js
+++ script.js
@@ -1,125 +1,67 @@
 
-const boomAudio = new Audio("./sounds/boom.wav");
-const clapAudio = new Audio("./sounds/clap.wav");
-const hihatAudio = new Audio("./sounds/hihat.wav");
-const kickAudio = new Audio("./sounds/kick.wav");
-const openhatAudio = new Audio("./sounds/openhat.wav");
-const rideAudio = new Audio("./sounds/ride.wav");
-const snareAudio = new Audio("./sounds/snare.wav");
-const tinkAudio = new Audio("./sounds/tink.wav");
-const tomAudio = new Audio("./sounds/tom.wav");
-
-
-const boomButton = document.getElementById("boomButton");
-const clapButton = document.getElementById("clapButton");
-const hihatButton = document.getElementById("hihatButton");
-const kickButton = document.getElementById("kickButton");
-const openhatButton = document.getElementById("openhatButton");
-const rideButton = document.getElementById("rideButton");
-const snareButton = document.getElementById("snareButton");
-const tinkButton = document.getElementById("tinkButton");
-const tomButton = document.getElementById("tomButton");
-
-
-function playBoom() {
-    boomAudio.currentTime = 0;
-    boomAudio.play();
-}
-const playClap = () => {
-    clapAudio.currentTime = 0;
-    clapAudio.play();
-}
-const playHihat = () => {
-    hihatAudio.currentTime = 0;
-    hihatAudio.play();
-}
-const playKick = () => {
-    kickAudio.currentTime = 0;
-    kickAudio.play();
-}
-const playOpenhat = () => {
-    openhatAudio.currentTime = 0;
-    openhatAudio.play();
-}
-const playRide = () => {
-    rideAudio.currentTime = 0;
-    rideAudio.play();
-}
-const playSnare = () => {
-    snareAudio.currentTime = 0;
-    snareAudio.play();
-}
-const playTink = () => {
-    tinkAudio.currentTime = 0;
-    tinkAudio.play();
-}
-const playTom = () => {
-    tomAudio.currentTime = 0;
-    tomAudio.play();
-}
-
-boomButton.addEventListener("click", playBoom)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "1") {
-        playBoom();
-    }
-})
-
-clapButton.addEventListener("click", playClap)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "2") {
-        playClap();
-    }
-})
-
-hihatButton.addEventListener("click", playHihat)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "3") {
-        playHihat();
-    }
-})
-
-kickButton.addEventListener("click", playKick)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "4") {
-        playKick();
-    }
-})
-
-openhatButton.addEventListener("click", playOpenhat)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "5") {
-        playOpenhat();
-    }
-})
-
-rideButton.addEventListener("click", playRide)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "6") {
-        playRide();
-    }
-})
-
-snareButton.addEventListener("click", playSnare)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "7") {
-        playSnare();
-    }
-})
-
-tinkButton.addEventListener("click", playTink)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "8") {
-        playTink();
-    }
-})
-
-tomButton.addEventListener("click", playTom)
-document.addEventListener("keydown", (event) => {
-    if (event.key == "9") {
-        playTom();
-    }
-})
-
-
-
+const allSoundEffects = {
+    boom: {
+      audio: new Audio("./sounds/boom.wav"),
+      button: document.getElementById("boomButton"),
+      key: "1",
+    },
+    clap: {
+      audio: new Audio("./sounds/clap.wav"),
+      button: document.getElementById("clapButton"),
+      key: "2",
+    },
+    hihat: {
+      audio: new Audio("./sounds/hihat.wav"),
+      button: document.getElementById("hihatButton"),
+      key: "3",
+    },
+    kick: {
+      audio: new Audio("./sounds/kick.wav"),
+      button: document.getElementById("kickButton"),
+      key: "4",
+    },
+    openhat: {
+      audio: new Audio("./sounds/openhat.wav"),
+      button: document.getElementById("openhatButton"),
+      key: "5",
+    },
+    ride: {
+      audio: new Audio("./sounds/ride.wav"),
+      button: document.getElementById("rideButton"),
+      key: "6",
+    },
+    snare: {
+      audio: new Audio("./sounds/snare.wav"),
+      button: document.getElementById("snareButton"),
+      key: "7",
+    },
+    tink: {
+      audio: new Audio("./sounds/tink.wav"),
+      button: document.getElementById("tinkButton"),
+      key: "8",
+    },
+    tom: {
+      audio: new Audio("./sounds/tom.wav"),
+      button: document.getElementById("tomButton"),
+      key: "9",
+    },
+  };
+  
+  const playSound = (sound) => {
+    sound.audio.currentTime = 0;
+    sound.audio.play();
+  }
+  
+  
+  const setupSoundEffect = (drumSound) => {
+    drumSound.button.addEventListener("click", () => playSound(drumSound));
+    document.addEventListener("keydown", (event) => {
+      if (event.key === drumSound.key) {
+        playSound(drumSound);
+      }
+    });
+  }
+  
+  for (const key in allSoundEffects) {
+    setupSoundEffect(allSoundEffects[key]);
+  }
